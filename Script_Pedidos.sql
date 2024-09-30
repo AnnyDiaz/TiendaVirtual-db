a4248e62c78b1aecffb13d5e0563f2c82b564524
AnnyDiaz <anayulizaquintero2001@gmail.comgit config --global user.name AnnyDiazgit config --global user.email anayulizaquintero2001@gmail.com>
2024-09-30 17:26:28 -0500

    se crea el script con los sp de la tienda virtual, de la tabla pedidos

---
+++ Script_Pedidos.sql
@@ -0,0 +1,51 @@
+-- Pedidos
+-- Insertar 
+DELIMITER //
+CREATE PROCEDURE procInsertPedidos(
+    IN v_fecha DATETIME,
+    IN v_estado VARCHAR(50),
+    IN v_total DOUBLE,
+    IN v_cliente_id INT
+)
+BEGIN
+    INSERT INTO tbl_pedidos (pedi_fecha, pedi_estado, pedi_total, tbl_clientes_cli_id)
+    VALUES (v_fecha, v_estado, v_total, v_cliente_id);
+END //
+DELIMITER ;
+
+-- Actualizar
+DELIMITER //
+CREATE PROCEDURE procUpdatePedidos(
+    IN v_id INT,
+    IN v_fecha DATETIME,
+    IN v_estado VARCHAR(50),
+    IN v_total DOUBLE,
+    IN v_cliente_id INT
+)
+BEGIN
+    UPDATE tbl_pedidos
+    SET 
+        pedi_fecha = v_fecha, 
+        pedi_estado = v_estado, 
+        pedi_total = v_total, 
+        tbl_clientes_cli_id = v_cliente_id
+    WHERE pedi_id = v_id;
+END //
+DELIMITER ;
+
+-- Mostrar
+DELIMITER //
+CREATE PROCEDURE procSelectPedidos()
+BEGIN
+    SELECT pedi_id, pedi_fecha, pedi_estado, pedi_total, tbl_clientes_cli_id
+    FROM tbl_pedidos;
+END //
+DELIMITER ;
+
+-- Eliminar
+DELIMITER //
+CREATE PROCEDURE procDeletePedidos(IN v_id INT)
+BEGIN
+    DELETE FROM tbl_pedidos WHERE pedi_id = v_id;
+END //
+DELIMITER ;
